05e36dc873e557aede51bb233357b3716ba68d38
nandabalassoni <mariafernandabalassoni@hotmail.com>
2022-07-03 20:02:13 -0300

    implementando a transferencia de dados, assim conseguimos imprimir os dados de um aluno escolhido da lista no formulario

--- app/src/main/java/com/example/agenda_android/ui/activity/FormularioCadastroAlunoActivity.java
+++ app/src/main/java/com/example/agenda_android/ui/activity/FormularioCadastroAlunoActivity.java
@@ -1,5 +1,6 @@
 package com.example.agenda_android.ui.activity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.example.agenda_android.R;
 import com.example.agenda_android.dao.AlunoDAO;
 import com.example.agenda_android.model.Aluno;
+
+import java.io.Serializable;
 
 public class FormularioCadastroAlunoActivity extends AppCompatActivity {
 
@@ -27,6 +30,12 @@ public class FormularioCadastroAlunoActivity extends AppCompatActivity {
         setTitle(APPBAR_TITLE);
         inicializacaoCampos();
         configuraBotaoSalvar();
+
+        Intent dadosColetadosAlunos = getIntent();
+        Aluno aluno = (Aluno) dadosColetadosAlunos.getSerializableExtra("aluno");
+        campoNome.setText(aluno.getNome());
+        campoTelefone.setText(aluno.getTelefone());
+        campoEmail.setText(aluno.getEmail());
     }
 
     private void configuraBotaoSalvar() {
--- app/src/main/java/com/example/agenda_android/ui/activity/ListaAlunosActivity.java
+++ app/src/main/java/com/example/agenda_android/ui/activity/ListaAlunosActivity.java
@@ -2,16 +2,22 @@ package com.example.agenda_android.ui.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.agenda_android.R;
 import com.example.agenda_android.dao.AlunoDAO;
+import com.example.agenda_android.model.Aluno;
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+import java.util.List;
 
 public class ListaAlunosActivity extends AppCompatActivity {
 
@@ -24,6 +30,7 @@ public class ListaAlunosActivity extends AppCompatActivity {
         setContentView(R.layout.activity_lista_alunos);
         setTitle(APPBAR_TITLE);
         configuraFabNovoAluno();
+        dao.salva(new Aluno("Alex", "1122223333", "alex@alura.com.br"));
     }
 
     private void configuraFabNovoAluno() {
@@ -48,9 +55,19 @@ public class ListaAlunosActivity extends AppCompatActivity {
 
     private void configuraLista() {
         ListView listaAlunos = findViewById(R.id.activity_lista_alunos_listview);
+        final List<Aluno> alunos = dao.todos();
         listaAlunos.setAdapter(new ArrayAdapter<>(
                 this,
                 android.R.layout.simple_list_item_1,
-                dao.todos()));
+                alunos));
+        listaAlunos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int posicao, long id) {
+                Aluno alunoEscolhido = alunos.get(posicao);
+                Intent chamaFormularioActivity = new Intent(ListaAlunosActivity.this, FormularioCadastroAlunoActivity.class);
+                chamaFormularioActivity.putExtra("aluno", alunoEscolhido);
+                startActivity(chamaFormularioActivity);
+            }
+        });
     }
 }
